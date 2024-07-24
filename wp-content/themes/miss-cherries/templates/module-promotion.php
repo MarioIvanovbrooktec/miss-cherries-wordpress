abb5e083412168222961b3176e7de3e3dd3904be
Mario Danov <mario.ivanov@brooktec.com>
2024-07-24 09:36:30 +0200

    Implemented promotion module fully

--- wp-content/themes/miss-cherries/templates/module-promotion.php
+++ wp-content/themes/miss-cherries/templates/module-promotion.php
@@ -12,11 +12,11 @@ if (!defined('ABSPATH')) {
                 <?php while (have_rows('footer', 'option')) :
                     the_row(); ?>
                     <?php if (get_row_layout() == 'promotion_section') : ?>
-                        <div class="col-10 offset-1">
-                            <div class="py-5 promotion-desc ">
+                        <div class="container col-10 offset-1">
+                            <div class="container d-flex justify-content-center py-5 promotion-desc ">
                                 <?php the_sub_field('description_promotion'); ?>
                             </div>
-                            <div class="d-flex justify-content-center pb-5">
+                            <div class="container d-flex justify-content-center pb-5">
                                 <?php the_sub_field('slogans_promotion'); ?>
                             </div>
                         </div>
