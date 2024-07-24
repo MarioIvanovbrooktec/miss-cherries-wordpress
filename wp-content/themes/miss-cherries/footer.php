5c82735d8d66070772e55d1c5a86a5326a692a44
Mario Danov <mario.ivanov@brooktec.com>
2024-07-24 17:29:57 +0200

    Fixed flexible content

--- wp-content/themes/miss-cherries/footer.php
+++ wp-content/themes/miss-cherries/footer.php
@@ -6,9 +6,20 @@ if (!defined('ABSPATH')) {
 
 ?>
         <footer>
-            <?php get_template_part('templates/module', 'newsletter'); ?>
-            <?php get_template_part('templates/module', 'promotion'); ?>
-            <?php get_template_part('templates/module', 'instagram'); ?>
+            <?php if (have_rows('footer', 'option')) : ?>
+                <?php while (have_rows('footer', 'option')) :
+                    the_row(); ?>
+                    <?php if (get_row_layout() == 'newsletter_section') : ?>
+                        <?php get_template_part('templates/module', 'newsletter'); ?>
+                    <?php endif; ?>
+                    <?php if (get_row_layout() == 'promotion_section') : ?>
+                        <?php get_template_part('templates/module', 'promotion'); ?>
+                    <?php endif; ?>
+                    <?php if (get_row_layout() == 'social_section') : ?>
+                        <?php get_template_part('templates/module', 'instagram'); ?>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
             <?php get_template_part('templates/module', 'social-links'); ?>
             <div id="wp-scripts">
                 <?php wp_footer(); ?>
--- wp-content/themes/miss-cherries/templates/module-instagram.php
+++ wp-content/themes/miss-cherries/templates/module-instagram.php
@@ -5,37 +5,30 @@ if (!defined('ABSPATH')) {
 }
 ?>
 <section class="module-instagram">
-    <?php if (have_rows('footer', 'option')) : ?>
-        <?php while (have_rows('footer', 'option')) :
-            the_row(); ?>
-            <?php if (get_row_layout() == 'social_section') : ?>
-                <div class="promotion-overlap">
-                    <div class="container col-1 d-flex justify-content-center">
-                        <div class="black-box-div ">
-                            <?php the_sub_field('button_text_social'); ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="img-overlap">
-                    <div class="container d-none d-md-block d-flex justify-content-center">
-                        <?php $image = get_sub_field('image_social'); ?>
-                        <img class="img-fluid" src="<?php echo $image;?>">
-                    </div>
-                    <div class="container-fluid d-block d-md-none d-flex p-0 justify-content-center">
-                        <?php $image = get_sub_field('image_social'); ?>
-                        <img class="img-fluid" src="<?php echo $image;?>">
-                    </div>
-                </div>
-                <div class="container d-flex flex-column align-items-center">
-                    <?php if (get_sub_field('instagram_handle_social')) : ?>
-                        <div class="mt-3 mb-2">SÍGUENOS</div>
-                        <div class="insta-handle">
-                        <i style="font-size: 1.2rem" class="fab fa-instagram"></i>
-                        <?php the_sub_field('instagram_handle_social'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            <?php endif; ?>
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <div class="promotion-overlap">
+        <div class="container col-1 d-flex justify-content-center">
+            <div class="black-box-div ">
+                <?php the_sub_field('button_text_social'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="img-overlap">
+        <div class="container d-none d-md-block d-flex justify-content-center">
+            <?php $image = get_sub_field('image_social'); ?>
+            <img class="img-fluid" src="<?php echo $image;?>">
+        </div>
+        <div class="container-fluid d-block d-md-none d-flex p-0 justify-content-center">
+            <?php $image = get_sub_field('image_social'); ?>
+            <img class="img-fluid" src="<?php echo $image;?>">
+        </div>
+    </div>
+    <div class="container d-flex flex-column align-items-center">
+        <?php if (get_sub_field('instagram_handle_social')) : ?>
+            <div class="mt-3 mb-2">SÍGUENOS</div>
+            <div class="insta-handle">
+            <i style="font-size: 1.2rem" class="fab fa-instagram"></i>
+            <?php the_sub_field('instagram_handle_social'); ?>
+            </div>
+        <?php endif; ?>
+    </div>
 </section>
