f80e22a7b00bbea73fca4dfc79533af2d09ff7eb
Mario Danov <mario.ivanov@brooktec.com>
2024-07-25 17:10:58 +0200

    Improvec newsletter module styling

--- wp-content/themes/miss-cherries/templates/module-newsletter.php
+++ wp-content/themes/miss-cherries/templates/module-newsletter.php
@@ -5,56 +5,54 @@ if (!defined('ABSPATH')) {
 }
 ?>
 <section class="module-newsletter">
-    <div class="container col-md-4 col-sm-10 d-flex flex-column align-items-center">
-        <div class="title-newsletter-main">
-            <?php the_sub_field('title_main'); ?>
-        </div>
-        <div class="description-newsletter-main">
+    <div class="title-newsletter-main">
+        <?php the_sub_field('title_main'); ?>
+    </div>
+    <div class="description-newsletter-main">
         <?php the_sub_field('description_main'); ?>
-        </div>
-        <div class=" container-fluid my-3">
-            <input type="email" class="form-control input-form" id="email-input-form" placeholder="">
-                <div class="invalid-feedback" id="empty-email">
+    </div>
+    <div class="input-holder">
+        <input type="email" class="form-control input-form" id="email-input-form" placeholder="">
+        <div class="invalid-feedback" id="empty-email">
             Es necesario introducir el email
-            </div>
-            <div class="invalid-feedback" id="invalid-email">
-                El correo no es valido
-            </div>
         </div>
-        <div>
-            <button type="button" class="black-box-div mb-5" id="sign-up-button">
-                <?php the_sub_field('button_text_main'); ?>
-            </button>
+        <div class="invalid-feedback" id="invalid-email">
+            El correo no es valido
         </div>
-        <div
-            class="modal fade"
-            id="myModal" tabindex="-1"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">
-                        <?php the_sub_field('title_modal'); ?>
-                    </h1>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close">
-                    </button>
-                    </div>
-                    <div class="modal-body">
-                    <?php the_sub_field('description_modal'); ?>
-                    </div>
-                    <div class="modal-footer">
-                    <button
+    </div>
+    <div>
+        <button type="button" class="black-box-div mb-5" id="sign-up-button">
+            <?php the_sub_field('button_text_main'); ?>
+        </button>
+    </div>
+    <div
+        class="modal fade"
+        id="myModal" tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                    <?php the_sub_field('title_modal'); ?>
+                </h1>
+                <button
                     type="button"
-                    class="btn btn-primary modal-close-button"
-                    data-bs-dismiss="modal">
-                        <?php the_sub_field('button_text_modal'); ?>
-                    </button>
-                    </div>
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close">
+                </button>
+                </div>
+                <div class="modal-body">
+                <?php the_sub_field('description_modal'); ?>
+                </div>
+                <div class="modal-footer">
+                <button
+                type="button"
+                class="btn btn-primary modal-close-button"
+                data-bs-dismiss="modal">
+                    <?php the_sub_field('button_text_modal'); ?>
+                </button>
                 </div>
             </div>
         </div>
