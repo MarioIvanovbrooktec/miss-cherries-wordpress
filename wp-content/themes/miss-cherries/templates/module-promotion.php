917b663d47c9c2a8ba2db526934e9250365d412a
Mario Danov <mario.ivanov@brooktec.com>
2024-07-26 09:42:56 +0200

    Improved promotion module styling

--- wp-content/themes/miss-cherries/templates/module-promotion.php
+++ wp-content/themes/miss-cherries/templates/module-promotion.php
@@ -6,25 +6,17 @@ if (!defined('ABSPATH')) {
 
 ?>
 <section class="module-promotion">
-    <div class="promotion-div">
-        <div class="container">
-            <div class=" col-md-10 offset-md-1 col-sm-12">
-                <div class="d-flex justify-content-center py-5">
-                    <div class="promotion-desc">
-                        <?php the_sub_field('description_promotion'); ?>
-                    </div>
+    <div class="promotion-desc">
+        <?php the_sub_field('description_promotion'); ?>
+    </div>
+    <div class="slogans-div">
+        <?php if (have_rows('slogans_promotion')) : ?>
+            <?php while (have_rows('slogans_promotion')) :
+                the_row(); ?>
+                <div>
+                    <?php the_sub_field('slogan_repeater'); ?>
                 </div>
-                <div class="slogans-div">
-                    <?php if (have_rows('slogans_promotion')) : ?>
-                        <?php while (have_rows('slogans_promotion')) :
-                            the_row(); ?>
-                            <div>
-                                <?php the_sub_field('slogan_repeater'); ?>
-                            </div>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-        </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 </section>
