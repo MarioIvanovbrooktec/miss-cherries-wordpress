553a937f585c0fdde7e68640d0c8dab0a3c70ba4
Mario Danov <mario.ivanov@brooktec.com>
2024-08-01 09:18:47 +0200

    Changed categories implementation from flex to grid

--- wp-content/themes/miss-cherries/templates/module-front-page-categories.php
+++ wp-content/themes/miss-cherries/templates/module-front-page-categories.php
@@ -15,30 +15,23 @@ if (!defined('ABSPATH')) {
         'taxonomy' => 'product_categories',
         'hide_empty' => false,
         ));
-        $mod = (count($terms) % 3) + 1;
         ?>
-    <?php for ($i = 0; $i < $mod; $i++) : ?>
-        <div class="category-rows">
-            <?php for ($j = 0; $j < 3; $j++) : ?>
-                <?php if (($j + $mod * $i) < count($terms)) : ?>
-                    <?php $current_term = $terms[($j + $mod * $i)]; ?>
-                    <div class="category">
-                        <a href="https://localhost:3000/front-page/">
-                            <div class="category-image">
-                                <?php $image = get_field('category_image', $current_term); ?>
-                                <img class="img-fluid" src="<?php echo $image;?>" alt="Cover Image">
-                            </div>
-                            <div class="category-name">
-                                <?php $name = $current_term -> name; ?>
-                                <div class="black-box-div">
-                                    <?php echo $name; ?>
-                                </div>
-                            </div>
-                        </a>
+    <div class="grid-container">
+        <?php foreach ($terms as $current_term) :  ?>
+            <div class="category">
+                <a href="https://localhost:3000/front-page/">
+                    <div class="category-image">
+                        <?php $image = get_field('category_image', $current_term); ?>
+                        <img class="img-fluid" src="<?php echo $image;?>" alt="Cover Image">
                     </div>
-                <?php endif; ?>
-            <?php endfor; ?>
-        </div>
-    <?php endfor; ?>
-
+                    <div class="category-name">
+                        <?php $name = $current_term -> name; ?>
+                        <div class="black-box-div">
+                            <?php echo $name; ?>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </section>
